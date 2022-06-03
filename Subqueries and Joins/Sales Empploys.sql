52257461f09d2f05fc61e4e47657086bfa5a9cab
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-06-03 11:29:38 +0100

    Subqueries and Joins

---
+++ Subqueries and Joins/Sales Empploys.sql
@@ -0,0 +1,7 @@
+SELECT 
+[EmployeeID],[FirstName],[LastName],[d].[Name] AS [DepartmentName]
+      FROM [Employees] AS [e]
+ LEFT JOIN [Departments] AS [d]
+        ON [e].DepartmentID=d.DepartmentID
+	WHERE [d].Name = 'Sales'
+  ORDER BY [EmployeeID] 
